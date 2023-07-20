0bd535f9e2550efb9bfd5d2858fe529859407924
Bel v <belenventurini@gmail.com>
2023-07-20 03:17:48 -0300

    nuevo commit

--- main.js
+++ main.js
@@ -18,15 +18,25 @@ const te3 = new Te('negro', 'energetico', 'amargo', 550, 'oscuro', 3);
 //hago un array vacio, en donde después a taves del metodo push, inserto mis objetos ahi adentro
 const listaDeTes = []
 
+
 agregarTesALista(te1);
 agregarTesALista(te2);
 agregarTesALista(te3);
 
 //mediante un prompt solicito al usuario que elija según el numero, uno de los tres objetos 
-let te = prompt("Elija un numero de Té: (frutos del bosque = 1 ,matcha = 2 ,negro = 3)")
-traerTePorID(te)
+// let te = prompt("Elija un numero de Té: (frutos del bosque = 1 ,matcha = 2 ,negro = 3)")
+
+let cajaTeElegido = document.getElementById("cajaTeElegido");
+const boton2 = document.getElementById('boton2');
+const boton3 = document.getElementById('boton3');
 
 
+boton2.addEventListener('click', () => {
+    traerTePorID(2); 
+});
+boton3.addEventListener('click', () => {
+    traerTePorID(3); 
+});
 
 //funcion con metodo push para insertar mis tres objetos dentro del array
 function agregarTesALista(te) {
@@ -37,25 +47,16 @@ function agregarTesALista(te) {
 //utilizo el metodo forEach que recorre mi array, y en la condición compara mi objeto por el id, con la validacion
 // del dato ingresado, mediante prompt  
 function traerTePorID(numero) {
-    //funcion validarIngreso la guerdo en mi variable numeroValido, para así poder comparar con mi objeto
-    let numeroValidado = validarIngreso(numero)
-    
-    listaDeTes.forEach(cajaDeTes => {
-        if (cajaDeTes.id == numeroValidado) {
-            console.log(cajaDeTes)
+
+    listaDeTes.forEach(cajaDeTe => {
+        if (cajaDeTe.id == numero) {
+            const mensaje = `Nombre: ${cajaDeTe.nombre}\nDescripción: ${cajaDeTe.funcion}\nPrecio: ${cajaDeTe.precio}`;
+            cajaTeElegido.textContent = mensaje;
+            console.log(mensaje)
         }
     });
 }
-//funcion que valida el dato ingresado por el usuario a traves de prompt
-//en caso de escribir un numero incorrecto 
-function validarIngreso(numero) {
-    while (numero < 1 || numero > 3 || numero == "") 
-    {
-        alert("ese numero no está en el menu")
-        numero = prompt("Elija un numero de Té: (frutos del bosque = 1 ,matcha = 2 ,negro = 3)")
-    }
 
-    return numero
-}
+
 
 
